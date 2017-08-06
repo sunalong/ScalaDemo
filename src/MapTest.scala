37246914845b855cab0872ac0bd9cd60b282b0ca
sunalong <sunalong@ztgame.com>
2017-08-06 20:50:54 +0800

    映射创建、获取值、修改值、追加元素

--- src/MapTest.scala
+++ src/MapTest.scala
@@ -1,18 +1,32 @@
+import scala.collection.mutable.Map
 /**
   * Created by along on 17/8/6 20:41.
   * Email:466210864@qq.com
   */
 object MapTest {
   def main(args: Array[String]): Unit = {
-    createMap
+    mapTest
   }
+
+  def mapTest: Unit ={
   //创建map的两行形式
-  def createMap: Unit ={
     val scores1 = Map("tom"->98,"jerry"->89,"kitty"->97)
     println("scores1:"+scores1)
 
     val scores2 = Map(("tom",98),("jerry",89),("kitty",90))
     println("scores2:"+scores2)
 
+    //获取映射中的值
+    println("tom:"+scores1("tom"))
+    println("tom:"+scores1.getOrElse("tom",22))
+    println("Nom:"+scores1.getOrElse("Nom",22))
+
+    //scala中有两种map,一个是immutable包下的map,该map中的内容不可变
+    //另一个是mutable包下的map，该map中的内容可变
+    scores1("tom")=100
+    println("修改后tom:"+scores1("tom"))
+
+    scores1 +=("fuck"->45,"suke"->79)
+    println("scores1:"+scores1)
   }
 }
