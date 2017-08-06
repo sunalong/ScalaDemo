8ecba93dbd940582674ed2e25eb9b5fe9354a612
sunalong <sunalong@ztgame.com>
2017-08-06 13:18:12 +0800

    函数与方法

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -8,6 +8,61 @@ object HelloWorld {
     println("Hello 2World")
     variable
     condition
+    blockExpression
+    ForRecir
+    val r1 = m1(f1)
+    println("r1=" + r1)
+
+    val r2 = m1(f2)
+    println("r2=" + r2)
+
+  }
+
+  private def m1(f: (Int, Int) => Int): Int = {
+    f(2, 6) + 3
+  }
+
+  val f1 = (x: Int, y: Int) => x + y
+  val f2 = (a: Int, b: Int) => a * b
+
+
+  private def ForRecir = {
+    //在scala中有for循环和while循环，用for循环比较多
+    //for循环语法结构：for(i<- 表达式/数组/集合）
+
+    //for(i<- 表达式），表达式 1 to 5 返回一个Range(区间)
+    //每次循环将区间中的一个值赋给i
+    for (i <- 1 to 5)
+      println("i=" + i)
+
+    //for(i<- 数组)
+    val arr = Array("a", "b", "c")
+    for (i <- arr)
+      println("i=" + i)
+
+    //高级for循环
+    //每个生成器都可以带一个条件，注意：if前面没有分号
+    for (i <- 1 to 3; j <- 1 to 3 if i != j)
+      println("i=" + i + " j=" + j)
+
+    //for推导式：如果for循环的循环体以yield开始，则该循环会构建出一个集合
+    //每次迭代生成集合中的一个值
+    val v = for (i <- 1 to 5) yield i * 10
+    println("v=" + v)
+  }
+
+  private def blockExpression = {
+    val x = 0
+    //在scala中{}中可包含一系列表达式，块中最后一个表达式的值就是块的返回值
+    val result = {
+      if (x < 0)
+        -1
+      else if (x >= 1)
+        1
+      else
+        "error"
+    }
+    println("result:" + result)
   }
 
   private def condition = {
@@ -41,6 +96,6 @@ object HelloWorld {
     //scala编译器会自动推断变量的类型，必要的时候可以指定类型
     //格式是：变量名：类型
     var str: String = "fuckme"
-    println("i:"+i+" s:"+s+" str:"+str)
+    println("i:" + i + " s:" + s + " str:" + str)
   }
 }
