d5805a00f4a3381c45f783c89cb401485ec37f00
sunalong <sunalong@ztgame.com>
2017-08-08 14:24:43 +0800

    偏函数

---
+++ src/com/itcode/first/PartialFuncDemo.scala
@@ -0,0 +1,26 @@
+package com.itcode.first
+
+/**
+  * Created by along on 17/8/8 12:35.
+  * Email:466210864@qq.com
+  */
+object PartialFuncDemo {
+  def main(args: Array[String]): Unit = {
+    println("func1:" + func1("one"))
+    println("func2:" + func1("two"))
+
+  }
+
+  def func1: PartialFunction[String, Int] = {
+    case "one" => 1
+    case "two" => 2
+    case _ => 1
+  }
+
+  def func2(num: String): Int = num match {
+    case "one" => 1
+    case "two" => 2
+    case _ => 1
+  }
+}
+
