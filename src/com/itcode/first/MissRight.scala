16ccaf9ad889c45fca659a088420d30e55ed2c8f
sunalong <sunalong@ztgame.com>
2017-08-08 11:09:18 +0800

    辅助构造器

--- src/com/itcode/first/MissRight.scala
+++ src/com/itcode/first/MissRight.scala
@@ -14,11 +14,20 @@ class MissRight {
   println(name)
   try {
     val context = Source.fromFile("/Users/along/GitHub/ScalaDemo/src/com/itcode/first/MissRight.scala").mkString
-    println("context:" + context)
+    println("context:")
   } catch {
     case e: Exception => e.printStackTrace
   } finally {
     println("finally")
+  }
+
+  private var gender = "male"
+  //用this关键字定义辅助构造器
+  def this(name:String,age:Int,gender:String){
+    //每个辅助构造器必须以主构造器或其他的辅助构造器的调用开始
+    this()
+    println("执行辅助构造器")
+    this.gender = gender
   }
 }
 
@@ -26,5 +35,7 @@ object MissRight {
   def main(args: Array[String]): Unit = {
     val missRight = new MissRight
     println("missRight:" + missRight)
+    val missThis = new MissRight("fuckName",18,"female")
+    println("missThis:" + missThis)
   }
 }
