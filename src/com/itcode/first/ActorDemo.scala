aefa94c1fc3dbc73be768bd9b883147ca2601272
sunalong <sunalong@ztgame.com>
2017-08-08 15:06:17 +0800

    actor接收消息

--- src/com/itcode/first/ActorDemo.scala
+++ src/com/itcode/first/ActorDemo.scala
@@ -8,23 +8,55 @@ import scala.actors.Actor
   */
 object ActorDemo {
   def main(args: Array[String]): Unit = {
+//    actorTest
+    actorReceiveTest
+  }
+
+  private def actorReceiveTest = {
+    val actor = new MyActor3
+    actor.start()
+    actor ! "start"
+    actor ! "stop"
+    println("消息发送完成！")
+  }
+
+  private def actorTest = {
     MyActor1.start()
     MyActor2.start()
   }
 }
 
+class MyActor3 extends Actor {
+  override def act(): Unit = {
+    while (true) {
+      receive {
+        case "start" =>
+          println("starting...")
+          Thread.sleep(1000)
+          println("started")
+
+        case "stop" =>
+          println("stopping...")
+          Thread.sleep(1000)
+          println("stopped")
+      }
+    }
+  }
+}
+
 object MyActor1 extends Actor {
-  override def act(): Unit ={
-    for(i<- 1 to 10){
-      println("actor-1:"+i)
+  override def act(): Unit = {
+    for (i <- 1 to 10) {
+      println("actor-1:" + i)
       Thread.sleep(1000)
     }
   }
 }
+
 object MyActor2 extends Actor {
-  override def act(): Unit ={
-    for(i<- 1 to 10){
-      println("actor-2:"+i)
+  override def act(): Unit = {
+    for (i <- 1 to 10) {
+      println("actor-2:" + i)
       Thread.sleep(1000)
     }
   }
