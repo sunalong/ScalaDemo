f8226cf51162d57fc482a17a1f128c7f63288ecf
sunalong <sunalong@ztgame.com>
2018-04-23 16:30:29 +0800

    异步获取数据

--- src/com/itcode/thread/ThreadDemo.scala
+++ src/com/itcode/thread/ThreadDemo.scala
@@ -1,5 +1,6 @@
 package com.itcode.thread
 
+import java.util
 import java.util.concurrent.{Callable, Executors, Future}
 
 import scala.util.Random
@@ -14,24 +15,34 @@ object ThreadDemo {
     * 线程池返回数据到主线程
     */
   def threadPoolWithRetData = {
-    val threadPool = Executors.newFixedThreadPool(2)
-    val future: Future[String] = threadPool.submit(new Callable[String] {
-      override def call() = {
-        println(Thread.currentThread().getName + " 执行任务并返回数据")
-        Thread.sleep(Random.nextInt(2000))
-        Thread.currentThread().getName + " 执行任务后返回的数据"
+    val threadPool = Executors.newFixedThreadPool(3)
+    var futureList = new util.ArrayList[Future[String]]()
+    for (i <- 1 to 10) {
+      val future: Future[String] = threadPool.submit(new CusCallable)
+      futureList.add(future)
+    }
+    while (futureList.size()>0) {
+      var itr0 = futureList.iterator();
+      while (itr0.hasNext()) {
+        var future = itr0.next();
+        if (future.isDone) {
+          println("主线程中获取线程池中的线程的返回值：" + future.get())
+          itr0.remove();
+        } else {
+          println("主线程中获取线程池中的线程的返回值：" + futureList.size())
+          Thread.sleep(1000)
+        }
       }
-    })
-    var status = future.isDone
-    println(s"task status:$status")
-
-    Thread.sleep(1000 * 3)
-    status = future.isDone
-    println(s"task status:$status")
-
-    if (status)
-      println("主线程中获取线程池中的线程的返回值：" + future.get())
+    }
     threadPool.shutdown()
+  }
+
+  class CusCallable extends Callable[String] {
+    override def call() = {
+      println(Thread.currentThread().getName + " 执行任务...")
+      Thread.sleep(Random.nextInt(3000))
+      "=====" + Thread.currentThread().getName + " 执行任务后返回的数据"
+    }
   }
 
   /**
